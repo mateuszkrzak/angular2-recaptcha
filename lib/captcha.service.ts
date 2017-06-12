1cbe4dbb6069c71f64aa24d9042ce5e40a1a2143
Mateusz Krzak <m.krzak@globkurier.pl>
2017-06-12 12:13:10 +0200

    add window var

--- lib/captcha.service.ts
+++ lib/captcha.service.ts
@@ -23,7 +23,7 @@ export class ReCaptchaService {
 
     constructor(zone: NgZone) {
         /* the callback needs to exist before the API is loaded */
-        window["reCaptchaOnloadCallback"] = <any> (() => zone.run(this.onloadCallback.bind(this)));
+        window[<any>"reCaptchaOnloadCallback"] = <any>(() => zone.run(this.onloadCallback.bind(this)));
     }
 
     public getReady(language: String): Observable<boolean> {
